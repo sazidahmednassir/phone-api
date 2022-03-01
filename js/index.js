e0a2929e8b8e9bff6da8eea995326e718a3466e6
sazidahmednassir <sazidahmednassir@gmail.com>
2022-03-01 10:17:47 -0800

    seven commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-//main container
+// container
 const main=document.getElementById('main');
+const detailsDv= document.getElementById('phone-details')
 
 const loadPhones=()=>{
     const error=document.getElementById('error');
@@ -20,11 +21,12 @@ else{
         fetch(`https://openapi.programming-hero.com/api/phones?search=${inputText}`)
         .then(res => res.json())
         .then ((phones)=> {
-            console.log(phones)
+            // console.log(phones)
             if (phones.status == false){
                 error.className="d-inline-block text-danger"
                error.innerText="There is no data, Search another brand phones";
                main.innerHTML=""
+               detailsDv.innerHTML=""
             } else{
                 error.className="d-none"
                 displayPhones(phones.data)
@@ -39,7 +41,7 @@ const displayPhones= phonesInfo=>{
 
     const phonesInfos = phonesInfo.slice(0,n);
    main.innerHTML=""
-   
+   detailsDv.innerHTML=""
     // console.log(phonesInfos);
     for (const  phone of phonesInfos ){
         // console.log(phone)
@@ -48,12 +50,13 @@ const displayPhones= phonesInfo=>{
         div.classList.add("col-md-4")
         div.classList.add('my-3')
         div.innerHTML=`
-        <div class="card " style="width: 18rem;">
-  <img src="${phone.image}" class="card-img-top img-fluid" alt="...">
+
+        <div class="card  text-center" style="width: 18rem;">
+  <img src="${phone.image}" class="card-img-top sp" alt="...">
   <div class="card-body">
     <h5 class="card-title">${phone.phone_name}</h5>
     <p class="card-text">${phone.brand}</p>
-    <button onclick="cardDeatils('${phone.slug}')" class="btn btn-primary"> See Details</buuton>
+    <button onclick="phoneDeatils('${phone.slug}')" class="btn btn-primary"> See Details</buuton>
   
   </div>
 </div>
@@ -62,3 +65,61 @@ const displayPhones= phonesInfo=>{
     
     }
 }
+
+const phoneDeatils=(id)=>{
+   const url=`https://openapi.programming-hero.com/api/phone/${id}`
+//    console.log(url);
+fetch(url)
+.then(res =>res.json())
+.then(phone=> displayPhonesDetails(phone.data))
+}
+
+const displayPhonesDetails=(details)=>{
+    console.log(details);
+  console.log(details.mainFeatures.sensors)
+  const n=details.mainFeatures.sensors.length;
+    detailsDv.innerHTML=""
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.classList.add('mt-3');
+    div.classList.add('sp');
+    div.innerHTML = `
+    <img src="${details.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title"> Name: ${details.name}</h5>
+        <p  class="card-text">Sensors:  ${details.mainFeatures.sensors.slice(0, n)}</p>
+        <ul class="list-group">
+        <li class="list-group-item ">WiFi: ${othersData(details?.others?.WLAN)}</li>
+        <li class="list-group-item ">Bluetooth: ${othersData(details?.others?.Bluetooth)}</li>
+        <li class="list-group-item ">GPS: ${othersData(details?.others?.GPS)}</li>
+        <li class="list-group-item ">NFC: ${othersData(details?.others?.NFC)}</li>
+        <li class="list-group-item ">Radio: ${othersData(details?.others?.Radio)}</li>
+        </ul>
+        <p  class="card-text">Release-Date:  ${releaseData ( details.releaseDate)}</p>
+       
+    </div>
+    `;
+    detailsDv.appendChild(div);
+   
+}
+
+//othersData function
+const othersData=(data)=>{
+    if(data==undefined){
+        return "No Data Found"
+    } else{
+        return data;
+    }
+}
+
+//releaseData function
+const releaseData=(date)=>{
+    if(date ==""){
+        return "No Date info Found"
+    }
+    else{
+        return date;
+    }
+}
+
+
